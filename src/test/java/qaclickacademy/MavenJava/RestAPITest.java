bbb618dea84d5af1c926c6476a42bcca96f32d6b
nitya555 <nityanandahu100@gmail.com>
2022-04-18 20:29:21 +0530

    second commit this commit from GitDemo IST

--- src/test/java/qaclickacademy/MavenJava/RestAPITest.java
+++ src/test/java/qaclickacademy/MavenJava/RestAPITest.java
@@ -6,10 +6,14 @@ public class RestAPITest {
 	@Test
 	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter1");
+		System.out.println("deleteTwitter2");
 	}
 	
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
 	}
 }
