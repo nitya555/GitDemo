2204e0e2da0fdcc7106265cbc6365391f3bcf15d
nitya555 <nityanandahu100@gmail.com>
2022-04-18 19:44:01 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+	
+	@Test
+	public void NativeAppsAndroid() {
+		System.out.println("NativeAppsAndroid");
+	}
+}
---
+++ src/test/java/qaclickacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementUi() {
+		System.out.println("elementUi");
+	}
+}
